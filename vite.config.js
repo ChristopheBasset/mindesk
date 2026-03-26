460aafd68029f9282588b49f16be62e7b78dbdbc
ChristopheBasset <christophe@comunecom.fr>
2026-03-26 18:08:57 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,10 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.svg'],
+      workbox: {
+        skipWaiting: true,
+        clientsClaim: true,
+      },
       manifest: {
         name: 'Mindesk',
         short_name: 'Mindesk',
